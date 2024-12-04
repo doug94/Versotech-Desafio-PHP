a18c3fffd60f5269cf8c183ffce8abfedde555aa
doug94 <douglas_dca@hotmail.com>
2024-12-04 18:49:12 -0300

    feat: initial create, update and delete implementations

--- connection.php
+++ connection.php
@@ -4,6 +4,7 @@ class Connection {
 
     private $databaseFile;
     private $connection;
+    private $endUserErrorMessage = "Não foi possível concluir a operação. ";
 
     public function __construct()
     {
@@ -13,7 +14,7 @@ class Connection {
 
     private function connect()
     {
-        return $this->connection = new PDO("sqlite:{$this->databaseFile}");
+        return $this->connection = new PDO("sqlite:{$this->databaseFile}", null, null, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
     }
 
     public function getConnection()
@@ -28,5 +29,45 @@ class Connection {
         $result->setFetchMode(PDO::FETCH_INTO, new stdClass);
 
         return $result;
+    }
+
+    public function insert($query, $user, $email, $colorId)
+    {
+        try
+        {
+            $statement = $this->getConnection()->prepare($query);
+            $statement->execute([
+                ':name' => $user,
+                ':email' => $email,
+                ':colorId' => $colorId
+            ]);
+        } 
+        catch (PDOException $exception) 
+        {
+            echo $this->endUserErrorMessage . $exception->getMessage();
+        }
+    }
+
+    public function update($query, $user, $email, $colorId)
+    {
+        try 
+        {
+            $statement = $this->getConnection()->prepare($query);
+            $statement->bindParam(':name', $user);
+            $statement->bindParam(':email', $email);
+            $statement->bindParam(':colorId', $colorId);
+            $statement->execute();
+        } 
+        catch (PDOException $exception) 
+        {
+            echo $this->endUserErrorMessage . $exception->getMessage();
+        }
+    }
+
+    public function delete($query, $userId)
+    {
+        $statement = $this->getConnection()->prepare($query);
+        $statement->bindParam(':userId', $userId, PDO::PARAM_INT);
+        $statement->execute();
     }
 }
