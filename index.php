6fb60a89056ba9cf34dac33759f74fa721dd089a
doug94 <douglas_dca@hotmail.com>
2024-12-05 16:41:10 -0300

    refactor: mixed PHP with HTML code to make form processing easier. feat: Added a container for adding new users

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
+<html>
+<head>
+<script>
+    function addNewUser() {
+        const newUser = document.createElement("input");
+        const newEmail = document.createElement("input");
+        const addButton = document.createElement("button");
+        newUser.setAttribute("type", "text");
+        addButton.setAttribute("type", "submit");
+        addButton.innerHTML = "Adicionar";
+        const container = document.getElementById("newUserContainer");
+        container.appendChild(newUser);
+        container.appendChild(newEmail);
+        container.appendChild(addButton);
+    }
+</script>
 <?php
 
 require 'connection.php';
@@ -6,29 +22,34 @@ $connection = new Connection();
 
 $users = $connection->query("SELECT * FROM users");
 
-echo "<table border='1'>
-
+?>
+</head>
+<body>
+<table border='1'>
     <tr>
         <th>ID</th>    
         <th>Nome</th>    
         <th>Email</th>
         <th>Ação</th>    
     </tr>
-";
 
-foreach($users as $user) {
+    <?php foreach ($users as $user) : ?>
+        <tr>
+            <td><?= $user->id; ?></td>
+            <td><?= $user->name; ?></td>
+            <td><?= $user->email; ?></td>
+            <td>
+                <a href="#">Editar</a>
+                <a href="#">Excluir</a>
+            </td>
+        </tr>
+    <?php endforeach; ?>
 
-    echo sprintf("<tr>
-                      <td>%s</td>
-                      <td>%s</td>
-                      <td>%s</td>
-                      <td>
-                           <a href='#'>Editar</a>
-                           <a href='#'>Excluir</a>
-                      </td>
-                   </tr>",
-        $user->id, $user->name, $user->email);
+</table>
+<br>
 
-}
+<button type="button" onclick="addNewUser()">Adicionar Usuário</button>
+<div id="newUserContainer"></div>
 
-echo "</table>";
+</body>
+</html>
